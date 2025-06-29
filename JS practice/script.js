7a7b55562cddaa4531b8d7e2191fe94dbe288650
CodeByMehra <kpvishalmehra@gmail.com>
2025-06-29 20:04:48 +0530

    Practice session

--- JS practice/script.js
+++ JS practice/script.js
@@ -2,4 +2,9 @@ let group= document.getElementsByClassName("box")
 console.log(group)
 
 group[2].style.backgroundColor= "blue"
-document.querySelector("box")
+document.querySelector("box") 
+
+let box4= document.getElementById("Box4")
+box4= box4.style.backgroundColor= "Orange"
+
+
