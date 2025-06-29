076452a8c94f48560f7ccf4cd1d8f5fb42f55978
CodeByMehra <kpvishalmehra@gmail.com>
2025-06-29 20:00:13 +0530

    Previous changes committed

---
+++ JS practice/basic.js
@@ -0,0 +1,12 @@
+console.log("Loops run started!!")
+
+
+for(i=0; i<=5; i++){
+    console.log(`for loop runned ${i} time`)
+}
+
+let a=1
+do{
+    console.log(`Do while loop runned ${a} time`)
+    a++
+} while(a<=5)
---
+++ Practice/trial.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter the value for A"
+    cin>>a;
+}
