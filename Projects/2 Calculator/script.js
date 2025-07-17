836e3711aa718980a60c97b3070479b3d065eda6
CodeByMehra <kpvishalmehra@gmail.com>
2025-07-17 16:36:58 +0530

    numerical buttons functionality done

--- Projects/2 Calculator/script.js
+++ Projects/2 Calculator/script.js
@@ -1,5 +1,30 @@
-let display= " ";
+// let display= ``;
 
-function buttonPress(num){
-    display =document.getElementsByClassName("display")[0].innerText = display +  `${num}` ;
+// function buttonPress(num){
+//     display =document.getElementsByClassName("display")[0].innerText = display +  `${num}` ;
+// }
+
+// function solution(display){
+//     let answer= eval(display);
+    
+//     document.getElementsByClassName("display")[0].innerText = `${answer}` ;
+//     display= answer;
+// }
+
+let displayText = ""; 
+
+function buttonPress(num) {
+    displayText = displayText + num;
+    document.getElementsByClassName("display")[0].innerText = displayText;
+}
+
+function solution() {
+    try {
+        let answer = eval(displayText);
+        document.getElementsByClassName("display")[0].innerText = answer;
+        displayText = `${answer}`; 
+    } catch (error) {
+        document.getElementsByClassName("display")[0].innerText = "Error";
+        displayText = "";
+    }
 }
