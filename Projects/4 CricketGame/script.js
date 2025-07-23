a88395bdd599fbfe1419d606615dcd7b6fa2d04d
CodeByMehra <kpvishalmehra@gmail.com>
2025-07-23 15:34:12 +0530

    Computer logic build

--- Projects/4 CricketGame/script.js
+++ Projects/4 CricketGame/script.js
@@ -1,4 +1,14 @@
 let userinput= "";
+let randomNumber= math.random()*3;
+let computerChoice = ""
+if (randomNumber>0 && randomNumber<1){
+    computerChoice = 'Bat';
+}else if(randomNumber>1 && randomNumber<2){
+    computerChoice = 'Ball';
+}else(randomNumber>2 && randomNumber<3){
+    computerChoice = 'Stump';
+};
+
 function getInput(userinput){
     document.getElementById("choiceDisplay").innerText= `You Choosen ${userinput} `;
 }
