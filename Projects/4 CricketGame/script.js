c3f1fee14fae660733be4478ac48ac0adcd6f8b0
CodeByMehra <kpvishalmehra@gmail.com>
2025-07-23 15:59:04 +0530

    Game logic working properly

--- Projects/4 CricketGame/script.js
+++ Projects/4 CricketGame/script.js
@@ -11,5 +11,28 @@ if (randomNumber>0 && randomNumber<1){
     computerChoice = 'Stump'
 };
     document.getElementById("choiceDisplay").innerText= `You Choosen ${userinput} And computer choosen ${computerChoice}`;
+    result(userinput,computerChoice);
+    
 }
 
+function result(userinput, computerChoice){
+    if(userinput == 'Bat' && computerChoice == 'Bat'){
+        document.getElementById("resultDisplay").innerText= `Result : Tie`;
+    }else if(userinput == 'Bat' && computerChoice == 'Ball'){
+        document.getElementById("resultDisplay").innerText= `Result : You Won`;
+    }else if(userinput == 'Bat' && computerChoice == 'Stump'){
+        document.getElementById("resulteDisplay").innerText= `Result : You Lost`;
+    }else if(userinput == 'Ball' && computerChoice == 'Bat'){
+        document.getElementById("resultDisplay").innerText= `Result : You Lost`;
+    }else if(userinput == 'Ball' && computerChoice == 'Ball'){
+        document.getElementById("resultDisplay").innerText= `Result : Tie`;
+    }else if(userinput == 'Ball' && computerChoice == 'Stump'){
+        document.getElementById("resultDisplay").innerText= `Result : You Won`;
+    }else if(userinput == 'Stump' && computerChoice == 'Bat'){
+        document.getElementById("resultDisplay").innerText= `Result : You Won`;
+    }else if(userinput == 'Stump' && computerChoice == 'Ball'){
+        document.getElementById("resultDisplay").innerText= `Result : You Lost`;
+    }else if(userinput == 'Stump' && computerChoice == 'Stump'){
+        document.getElementById("resultDisplay").innerText= `Result : Tie`;
+    }
+}
