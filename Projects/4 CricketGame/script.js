572dab4a6d5bf94cbc3ae2a46a81626bffdcb923
CodeByMehra <kpvishalmehra@gmail.com>
2025-08-19 19:42:23 +0530

    ScoreBoard Logic finalised

--- Projects/4 CricketGame/script.js
+++ Projects/4 CricketGame/script.js
@@ -1,20 +1,20 @@
-let userinput= "";
+let userinput = "";
 
-function getInput(userinput){
-    let randomNumber= Math.random()*3;
+function getInput(userinput) {
+    let randomNumber = Math.random() * 3;
     let computerChoice = ""
-if (randomNumber>0 && randomNumber<1){
-    computerChoice = 'Bat';
-}else if(randomNumber>1 && randomNumber<2){
-    computerChoice = 'Ball';
-}else if(randomNumber>2 && randomNumber<3){
-    computerChoice = 'Stump'
-};
-    document.getElementById("choiceDisplay").innerText= `Your Choice: ${userinput} 
+    if (randomNumber > 0 && randomNumber < 1) {
+        computerChoice = 'Bat';
+    } else if (randomNumber > 1 && randomNumber < 2) {
+        computerChoice = 'Ball';
+    } else if (randomNumber > 2 && randomNumber < 3) {
+        computerChoice = 'Stump'
+    };
+    document.getElementById("choiceDisplay").innerText = `Your Choice: ${userinput} 
     computer choosen: ${computerChoice}`;
     // result(userinput,computerChoice);
-    scoreBoard(userinput,computerChoice);
-    
+    scoreBoard(userinput, computerChoice);
+
 }
 
 // function result(userinput, computerChoice){
@@ -42,24 +42,42 @@ if (randomNumber>0 && randomNumber<1){
 let userScores = 0;
 let pcScores = 0;
 
-function scoreBoard(userinput,computerChoice){
-    if(userinput == 'Bat' && computerChoice == 'Bat'){
-        document.getElementById("resultDisplay").innerText= `Result : Tie`;
-    }else if(userinput == 'Bat' && computerChoice == 'Ball'){
-        document.getElementById("userScoreCount").innerText= userScores++;
-    }else if(userinput == 'Bat' && computerChoice == 'Stump'){
-        document.getElementById("pcScoreCount").innerText= pcScores++;
-    }else if(userinput == 'Ball' && computerChoice == 'Bat'){
-        document.getElementById("pcScoreCount").innerText= pcScores++;
-    }else if(userinput == 'Ball' && computerChoice == 'Ball'){
-        document.getElementById("resultDisplay").innerText= `Result : Tie`;
-    }else if(userinput == 'Ball' && computerChoice == 'Stump'){
-        document.getElementById("userScoreCount").innerText= userScores++;
-    }else if(userinput == 'Stump' && computerChoice == 'Bat'){
-        document.getElementById("userScoreCount").innerText= userScores++;
-    }else if(userinput == 'Stump' && computerChoice == 'Ball'){
-        document.getElementById("pcScoreCount").innerText= pcScores++;
-    }else if(userinput == 'Stump' && computerChoice == 'Stump'){
-        document.getElementById("resultDisplay").innerText= `Result : Tie`;
+
+
+function scoreBoard(userinput, computerChoice) {
+    if (userScores < 5 && pcScores < 5) {
+        if (userinput == 'Bat' && computerChoice == 'Bat') {
+            document.getElementById("resultDisplay").innerText = `Result : Tie`;
+        } else if (userinput == 'Bat' && computerChoice == 'Ball') {
+            userScores++;
+            document.getElementById("userScoreCount").innerText = userScores;
+        } else if (userinput == 'Bat' && computerChoice == 'Stump') {
+            pcScores++;
+            document.getElementById("pcScoreCount").innerText = pcScores;
+        } else if (userinput == 'Ball' && computerChoice == 'Bat') {
+            pcScores++;
+            document.getElementById("pcScoreCount").innerText = pcScores;
+        } else if (userinput == 'Ball' && computerChoice == 'Ball') {
+            document.getElementById("resultDisplay").innerText = `Result : Tie`;
+        } else if (userinput == 'Ball' && computerChoice == 'Stump') {
+            userScores++;
+            document.getElementById("userScoreCount").innerText = userScores;
+        } else if (userinput == 'Stump' && computerChoice == 'Bat') {
+            userScores++;
+            document.getElementById("userScoreCount").innerText = userScores;
+        } else if (userinput == 'Stump' && computerChoice == 'Ball') {
+            pcScores++;
+            document.getElementById("pcScoreCount").innerText = pcScores;
+        } else if (userinput == 'Stump' && computerChoice == 'Stump') {
+            document.getElementById("resultDisplay").innerText = `Result : Tie`;
+        }
+        // Check for win after updating scores
+        if (userScores === 5) {
+            document.getElementById("resultSection").innerText = `Result : You Won!! 
+           `;
+        } else if (pcScores === 5) {
+            document.getElementById("resultSection").innerText = `Result : PC WON!! 
+            `;
+        }
     }
 }
