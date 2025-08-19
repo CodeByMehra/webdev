a9a6833b81aa24f2c4d604566119af9e84fbf8ba
CodeByMehra <kpvishalmehra@gmail.com>
2025-08-19 19:16:03 +0530

    half scoreBoard done

--- Projects/4 CricketGame/script.js
+++ Projects/4 CricketGame/script.js
@@ -12,31 +12,54 @@ if (randomNumber>0 && randomNumber<1){
 };
     document.getElementById("choiceDisplay").innerText= `Your Choice: ${userinput} 
     computer choosen: ${computerChoice}`;
-    result(userinput,computerChoice);
+    // result(userinput,computerChoice);
+    scoreBoard(userinput,computerChoice);
     
 }
 
-function result(userinput, computerChoice){
+// function result(userinput, computerChoice){
+//     if(userinput == 'Bat' && computerChoice == 'Bat'){
+//         document.getElementById("resultDisplay").innerText= `Result : Tie`;
+//     }else if(userinput == 'Bat' && computerChoice == 'Ball'){
+//         document.getElementById("resultDisplay").innerText= `Result : You Won`;
+//     }else if(userinput == 'Bat' && computerChoice == 'Stump'){
+//         document.getElementById("resulteDisplay").innerText= `Result : You Lost`;
+//     }else if(userinput == 'Ball' && computerChoice == 'Bat'){
+//         document.getElementById("resultDisplay").innerText= `Result : You Lost`;
+//     }else if(userinput == 'Ball' && computerChoice == 'Ball'){
+//         document.getElementById("resultDisplay").innerText= `Result : Tie`;
+//     }else if(userinput == 'Ball' && computerChoice == 'Stump'){
+//         document.getElementById("resultDisplay").innerText= `Result : You Won`;
+//     }else if(userinput == 'Stump' && computerChoice == 'Bat'){
+//         document.getElementById("resultDisplay").innerText= `Result : You Won`;
+//     }else if(userinput == 'Stump' && computerChoice == 'Ball'){
+//         document.getElementById("resultDisplay").innerText= `Result : You Lost`;
+//     }else if(userinput == 'Stump' && computerChoice == 'Stump'){
+//         document.getElementById("resultDisplay").innerText= `Result : Tie`;
+//     }
+// }
+
+let userScores = 0;
+let pcScores = 0;
+
+function scoreBoard(userinput,computerChoice){
     if(userinput == 'Bat' && computerChoice == 'Bat'){
         document.getElementById("resultDisplay").innerText= `Result : Tie`;
     }else if(userinput == 'Bat' && computerChoice == 'Ball'){
-        document.getElementById("resultDisplay").innerText= `Result : You Won`;
+        document.getElementById("userScoreCount").innerText= userScores++;
     }else if(userinput == 'Bat' && computerChoice == 'Stump'){
-        document.getElementById("resulteDisplay").innerText= `Result : You Lost`;
+        document.getElementById("pcScoreCount").innerText= pcScores++;
     }else if(userinput == 'Ball' && computerChoice == 'Bat'){
-        document.getElementById("resultDisplay").innerText= `Result : You Lost`;
+        document.getElementById("pcScoreCount").innerText= pcScores++;
     }else if(userinput == 'Ball' && computerChoice == 'Ball'){
         document.getElementById("resultDisplay").innerText= `Result : Tie`;
     }else if(userinput == 'Ball' && computerChoice == 'Stump'){
-        document.getElementById("resultDisplay").innerText= `Result : You Won`;
+        document.getElementById("userScoreCount").innerText= userScores++;
     }else if(userinput == 'Stump' && computerChoice == 'Bat'){
-        document.getElementById("resultDisplay").innerText= `Result : You Won`;
+        document.getElementById("userScoreCount").innerText= userScores++;
     }else if(userinput == 'Stump' && computerChoice == 'Ball'){
-        document.getElementById("resultDisplay").innerText= `Result : You Lost`;
+        document.getElementById("pcScoreCount").innerText= pcScores++;
     }else if(userinput == 'Stump' && computerChoice == 'Stump'){
         document.getElementById("resultDisplay").innerText= `Result : Tie`;
     }
 }
-
-let userScores;
-let pcScores;
