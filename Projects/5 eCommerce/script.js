204c64cf63e8a8e1f1896f2da11f0881d419feb3
CodeByMehra <kpvishalmehra@gmail.com>
2025-08-25 18:58:44 +0530

    Add to cart funtion count completed

--- Projects/5 eCommerce/script.js
+++ Projects/5 eCommerce/script.js
@@ -2,7 +2,11 @@ let itemCount = 0;
 
 function addToCart(){
     itemCount = itemCount+1 
-    document.getElementById("cartItems").innerText= `Your cart has ${itemCount} items`;
+    document.getElementById("cartItemsCount").innerText= itemCount;
+    document.getElementById("cartItemsCount").style.display="inline";
+    if(itemCount>20){
+       document.getElementById("cartItemsCount").innerText= "20+"; 
+    }
 }
 
 
