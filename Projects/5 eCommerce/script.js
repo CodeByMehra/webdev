c22dab79927b7e7287a10aa2203bc7d82e809e85
CodeByMehra <kpvishalmehra@gmail.com>
2025-08-26 14:00:57 +0530

    Products removed from html

--- Projects/5 eCommerce/script.js
+++ Projects/5 eCommerce/script.js
@@ -10,3 +10,53 @@ function addToCart(){
 }
 
 
+const products = [
+  {
+    brand: "Zara",
+    name: "Boys Half Sleeve Shirt",
+    price: 1199,
+    mrp: 1900,
+    discount: "62% OFF",
+    image: "images/Boys_Shirt.jpg"
+  },
+  {
+    brand: "H&M",
+    name: "3 T-Shirts Combo Unisex",
+    price: 1249,
+    mrp: 2400,
+    discount: "55% OFF",
+    image: "images/Tshirt_Combo.webp"
+  },
+  {
+    brand: "US POLO",
+    name: "Men's Stripes Casual Shirt",
+    price: 1385,
+    mrp: 2100,
+    discount: "46% OFF",
+    image: "images/Stripes_Shirt.webp"
+  }
+];
+
+let itemCardsContainer = document.querySelector(".itemCards")
+
+for (i=0 ; i< products.length ; i++){
+    let card = document.createElement(div)
+    card.classlist.add("card")
+
+     card.innerHTML = `
+      <img src="${product.image}" alt="${product.name}">
+      <p class="itemBrand">${product.brand}</p>
+      <p>${product.name}</p>
+      <div class="pricingSection">
+          <span class="currentPrice">₹${product.price}</span>
+          <span class="mrp">₹${product.mrp}</span>
+          <span class="discount">${discount}% OFF</span>
+      </div>
+      <div class="actionButtons">
+          <button>Buy Now</button>
+          <button onclick="addToCart('${product.name}', ${product.price})">Add To Cart</button>
+      </div>
+  `;
+
+  itemCardsContainer.appendChild(card);
+}
