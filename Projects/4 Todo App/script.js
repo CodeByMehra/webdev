d380a543a29a525dba626e00f42ba8c8954dda4c
CodeByMehra <kpvishalmehra@gmail.com>
2025-08-31 20:32:36 +0530

    Rewriting the getinput function

--- Projects/4 Todo App/script.js
+++ Projects/4 Todo App/script.js
@@ -8,16 +8,16 @@ function getInput() {
     const taskDate = document.querySelector('.inputBox input[type="date"]').value;
 
     if (taskText && taskDate) {
-        document.querySelector(".outputBox").innerHTML = `
-               <h4>${taskText}</h4>
-               <h4>${taskDate}</h4>
-               <button onclick="deleteTask(this)">Delete</button>
-               `
+        const outputBox = document.querySelector(".outputBox");
+        const taskArea = document.createElement("section");
+        taskArea.className = "taskArea";
+        taskArea.innerHTML = `
+            <h4>${taskText}</h4>
+            <h4>${taskDate}</h4>
+            <button onclick="deleteTask(this)">Delete</button>
+        `;
+        
     }
-    outputBox.appendChild(outputBox);
-
-    taskText = document.querySelector('.inputBox input[type="text"]').value= " ";
-    taskDate = document.querySelector('.inputBox input[type="date"]').value= " ";
 }
 
 function deleteTask(btn) {
