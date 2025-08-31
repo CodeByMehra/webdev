e81d3ef5fecf652c3e9892917bab7dd1a287332a
CodeByMehra <kpvishalmehra@gmail.com>
2025-08-31 20:28:20 +0530

    Show to cart function removed

--- Projects/5 eCommerce/cart.js
+++ Projects/5 eCommerce/cart.js
@@ -13,30 +13,3 @@ let discountvalue = function (price , mrp){
         return discountvalue;
 }    
 
-function showToCart(name, price){
-    for (i=0 ; i<=cartItems.length; i++){
-        if(cartItems.name == name && cartItems.price==price){
-            return
-        }else{
-            let cartSection= document.querySelector(".addedItems")
-            let itemCard= document.createElement("Div")
-            itemCard.classList.add("itemContainer")
-            itemCard.innerHTML= `
-            <img src=${cartItems.image}alt="${cartItems.name}">
-            <div class="itemDetails">
-                <span class="itemBrand">${cartItems.brand}</span>
-                <span>${cartItems.name}</span>
-                <div class="pricingSection">
-                    <span class="currentPrice">${cartItems.price}</span>
-                    <span class="mrp">${cartItems.mrp}</span>
-                    <span class="discount">${discountvalue(product.price, product.mrp)}% OFF</span>
-                </div>
-                <span class="deliveryDate">Free Delivery By 3  Sep 2025</span>
-            </div>
-            `
-
-            cartSection.appendChild(itemCard)
-        }
-
-    }
-}
