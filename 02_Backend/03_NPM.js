995d1b950a2fa601f4c90e92e4e029ce81c5b573
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-19 15:55:59 +0530

    Figletdone

---
+++ 02_Backend/03_NPM.js
@@ -0,0 +1,10 @@
+//NPM: node package manager
+//it can be imagined as a library of packages
+//it is also a command line tool, means we can install packages in command line using npm
+
+//package: It can be defined as a code written by someone 
+
+//Packages can be installed usin npm by npm install command
+//EG: npm install figlet
+
+//Package.json file: this file contains descreptive and functional metadata about a project such as a name, version and dependencies
