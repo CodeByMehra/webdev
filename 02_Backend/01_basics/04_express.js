21c18e7ca0f1fe9847af86996c8eed0acca292f3
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-19 16:29:33 +0530

    added express intro

---
+++ 02_Backend/01_basics/04_express.js
@@ -0,0 +1,6 @@
+//library Vs Framework: 
+//library-  A Library is a collection off pre written code that can be used to perorm specific taks. eg:axios
+
+//Framework: it is a collection off pre written code that provides a structure for developing software applications eg:express
+
+// Express is a node.js web application that helps us to make web applications, used for server side programming.
