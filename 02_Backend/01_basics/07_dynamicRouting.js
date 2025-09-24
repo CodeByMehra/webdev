ef35280fb92b5970f67449169a026835ed936c35
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-24 15:20:02 +0530

    dynamic routing code added

---
+++ 02_Backend/01_basics/07_dynamicRouting.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("something about main page");
+})
+
+app.get("/about", function (req, res) {
+    res.send("something about about page");
+})
+
+app.get("/author/:username/:age", function (req, res) {
+    res.send(`something about ${req.params.username} page who is of ${req.params.age}`);
+})
+
+app.listen(3000);
