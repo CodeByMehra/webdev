6e569744041b318baf57abdfb9c5734d1c62f187
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-25 13:15:21 +0530

    Notes added for forms

---
+++ 02_Backend/03_ejs/00_notes.js
@@ -0,0 +1,11 @@
+//1. form jab bhi submit karoge pahli cheej ye dekhiye ki saari fields ke paas name propperty hai ya nahi
+
+//2. form ke paas action hai ya nahi
+
+//3.backend ke paas route hai ya nahi
+
+//4. wo route ka type sahi hai ya nahi
+
+//5. route ke andar aapko data unke type ke hisaab se milega
+    // GET - req.query
+    // POST - req.body
