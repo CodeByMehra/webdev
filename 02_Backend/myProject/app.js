01d757a881cc2a15fa0fa958970597ebba4df100
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-26 18:53:28 +0530

    date function

--- 02_Backend/myProject/app.js
+++ 02_Backend/myProject/app.js
@@ -1,15 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const fs = require('fs'); // Added file system module
+const path = require("path");
+const fs = require("fs");
 
-app.set('view engine', "ejs");
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("hey");
+  res.send("hey");
+});
+
+app.get("/create", (req, res) => {
+  const day = String(today.getDate()).padStart(2, "0");
+  const month = String(today.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed, so we add 1
+  const year = String(today.getFullYear()).slice(-2);
 });
 
 app.listen(3000);
