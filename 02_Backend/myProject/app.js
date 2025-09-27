f6125d55b09477812e4c26b49041872e2721acc6
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-27 14:34:44 +0530

    file reading dummy testing done

--- 02_Backend/myProject/app.js
+++ 02_Backend/myProject/app.js
@@ -9,7 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index", "files");
+  fs.readdir(`./files`, function(err,files){
+    res.render("index", {files});
+  })
 });
 
 app.get("/create", (req, res) => {
