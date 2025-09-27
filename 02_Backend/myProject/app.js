0d5b8c64c453e3995dccd12e3f890cbc8e47ebf6
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-27 14:23:00 +0530

    File reading system implementation dummy

--- 02_Backend/myProject/app.js
+++ 02_Backend/myProject/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("hey");
+  res.render("index", "files");
 });
 
 app.get("/create", (req, res) => {
