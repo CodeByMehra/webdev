e5db7a8a1ac78e5c79bb3327661057ab9a26edf5
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-27 14:14:06 +0530

    file creation testing done

--- 02_Backend/myProject/app.js
+++ 02_Backend/myProject/app.js
@@ -13,12 +13,20 @@ app.get("/", (req, res) => {
 });
 
 app.get("/create", (req, res) => {
+  const today = new Date();
   const day = String(today.getDate()).padStart(2, "0");
   const month = String(today.getMonth() + 1).padStart(2, "0"); 
   const year = String(today.getFullYear()).slice(-2);
 
   const date= `${day}-${month}-${year}.txt`
-  fs.writeFile(date, "check check")
+  fs.writeFile(`./files/${date}`, "check check",  (err) => {
+  if (err) {
+    console.error("Error creating file:", err);
+    return res.status(500).send("Error creating file");
+  }
+  res.send(`File ${date} created successfully!`);
+});
+  res.send(`File ${date} created successfully!`);
 });
 
-app.listen(3000);
+app.listen(3000); 
