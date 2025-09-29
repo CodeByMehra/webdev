b201de6ce66daf0b83659f74f15e62042545e171
CodeByMehra <kpvishalmehra@gmail.com>
2025-09-29 13:21:07 +0530

    routes created

--- 02_Backend/khatabook/app.js
+++ 02_Backend/khatabook/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require ("fs");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -8,7 +9,22 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req, res)=>{
+    fs.readdir("./files", function(err, files){
+        res.render("index" , {files: files})
+    })
+    
+});
+
+app.get("/edit", (req, res)=>{
     res.render("edit")
+});
+
+app.get("/create", (req, res)=>{
+    res.render("create")
+});
+
+app.get("/show", (req, res)=>{
+    res.render("show")
 });
 
 app.listen(3000);
