4fc686953fc77b4f3c48b804b9d5fe6fa3898841
CodeByMehra <kpvishalmehra@gmail.com>
2025-10-17 14:08:11 +0530

    updated schema details

--- 02_Backend/khatabook2/models/user-model.js
+++ 02_Backend/khatabook2/models/user-model.js
@@ -2,11 +2,29 @@ const mongoose = require("mongoose");
 const { number } = require("joi");
 mongoose.connect("mongodb:127.0.0.1:27017/joitestdb"); 
 const userSchema = mongoose.Schema({
-    username : string,
-    name : string,
-    age : number,
-    contact : number,
-    email : string,
+    username: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    age: {
+        type: Number,
+        min: 18,
+        required: true
+    },
+    contact: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: string,
+        required: true
+    }
 })
 
 module.exports = module.exports("User", userSchema)
