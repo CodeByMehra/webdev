cd3269125e65b838b1ccd9bd969332c7d2d6b2f6
CodeByMehra <kpvishalmehra@gmail.com>
2025-10-22 18:57:26 +0530

    started fllstack

--- 02_Backend/khatabook2/models/user-model.js
+++ 02_Backend/khatabook2/models/user-model.js
@@ -1,42 +1,62 @@
-const mongoose = require("mongoose");
-const { number } = require("joi");
-mongoose.connect("mongodb:127.0.0.1:27017/joitestdb"); 
-const userSchema = mongoose.Schema({
+// models/user.js
+
+// 1. Import dependencies
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+// 2. Connect to MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/joitestdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected successfully"))
+.catch(err => console.error("MongoDB connection error:", err));
+
+// 3. Define Mongoose schema
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        minLength: 3,
-        required: true
+        required: true,
+        minlength: 3
     },
     name: {
         type: String,
-        minLength: 3,
-        required: true
+        required: true,
+        minlength: 3
     },
     age: {
         type: Number,
-        min: 18,
-        required: true
+        required: true,
+        min: 18
     },
     contact: {
         type: Number,
         required: true
     },
     email: {
-        type: string,
+        type: String,
         required: true
     }
-})
+});
 
-function validateModel(data){
-    Joi.object({
+// 4. Create Mongoose model
+const userModel = mongoose.model("User", userSchema);
+
+// 5. Joi validation function
+function validateUser(data) {
+    const schema = Joi.object({
         username: Joi.string().min(3).required(),
         name: Joi.string().min(3).required(),
-        email: Joi.email().required(),
         age: Joi.number().min(18).required(),
-        contact: Joi.number().required()
-    })
-    let resolveans = Schema.validate(data);
-    return resolveans;
+        contact: Joi.number().required(),
+        email: Joi.string().email().required()
+    });
+
+    return schema.validate(data);
 }
-let userModel = module.exports("User", userSchema);
-module.exports = {validateModel, userModel}
+
+// 6. Export model and validation
+module.exports = {
+    userModel,
+    validateUser
+};
